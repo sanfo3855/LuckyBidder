eb1779ecf8aa30ceada0a440b4944035c61b1277
sanfe <sanfe@DESKTOP-1SP9KTN>
2017-10-06 00:29:44 +0200

    Spacing registrazione

--- LuckyBidder/src/com/luckybidder/client/Registrazione.java
+++ LuckyBidder/src/com/luckybidder/client/Registrazione.java
@@ -252,7 +252,6 @@ public class Registrazione extends HorizontalPanel{
 		});		
 		
 		
-		
 		decoratorpanelgrid.add(verticalpanel);
 		decoratorpanelgrid.getElement().setAttribute("style", "margin-left: 39vw; margin-top: 10vh;");
 		this.add(decoratorpanelgrid);

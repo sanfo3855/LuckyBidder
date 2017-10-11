5693430245515ec998fc12ecdc945c85806d15c3
sanfo3855 <sanfelicimatteo@gmail.com>
2017-10-11 11:58:39 +0200

    boh

--- LuckyBidder/src/com/luckybidder/client/LuckyBidder.java
+++ LuckyBidder/src/com/luckybidder/client/LuckyBidder.java
@@ -4,21 +4,17 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class LuckyBidder implements EntryPoint {
-	
+
 	public void onModuleLoad() {
-		
-		
+
+
 		final RootPanel rootpanel = RootPanel.get();
 		rootpanel.setSize("100%", "100%");
-		
+
 		final LuckyBidderServiceAsync instanceLuckyBidderService = LuckyBidderService.Util.getInstance();
-		
+
 		TopBar topbar = new TopBar();
 		rootpanel.add(topbar);
-<<<<<<< HEAD
-=======
-	
->>>>>>> e96c89081eca7db2b74332291fde6b00fc6b90d8
 
 		Login login = new Login();
 		rootpanel.add(login);
